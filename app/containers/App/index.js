773586947634cbf6ffa64edb1d4ec4f06f34119f
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2018-03-24 16:30:08 -0400

    add a navbar

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -15,15 +15,19 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
+import Navbar from 'containers/Navbar';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
 export default function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route component={NotFoundPage} />
-      </Switch>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
     </div>
   );
 }
