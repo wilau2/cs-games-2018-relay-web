4e1e6717f0a561048abd463a9e24d347ac80dff4
Tobi Décary-Larocque <tobdl09@hotmail.com>
2018-03-24 14:26:27 -0400

    display currency type and ammount

--- app/containers/Wallet/index.js
+++ app/containers/Wallet/index.js
@@ -41,6 +41,8 @@ class Wallet extends React.Component { // eslint-disable-line react/prefer-state
                 <div>
                   <div><span>address: </span><span>{wallet.address}</span></div>
                   <div><span>username: </span><span>{wallet.username }</span></div>
+                  <div><span>username: </span><span>{wallet.currency }</span></div>
+                  <div><span>username: </span><span>{wallet.amount }</span></div>
                 </div>
               </div>
             ))}
