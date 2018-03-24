4dfc4e3074931f1af8a1cedfc6ce4b23003c4811
leishaburford <lburf102@uottawa.ca>
2018-03-24 15:23:33 -0400

    added gabriels changes

--- app/containers/Wallet/index.js
+++ app/containers/Wallet/index.js
@@ -22,10 +22,55 @@ const styles = () => ({
   },
 });
 
-class Wallet extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <div>
+class Wallet extends React.Component { 
+  // eslint-disable-line react/prefer-stateless-function
+  // GABRIEL T: this and the following methods are used for wallect creation
+  // followed tutorial at https://reactjs.org/docs/forms.html
+  // This should probably go in a new file
+  // Submitting doesn't create a new file yet. Need to add ot the wallets file. See handleSubmit()
+  // I wish I knew react :')
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      currency: ''
+  };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const name = target.name;
+    this.setState({[name]: target.value});
+  }
+ 
+  handleSubmit(event) {
+    // CREATE & SAVE WALLET
+    alert('new submission: ' + this.state.username + ' ' + this.state.currency);
+    event.preventDefault();
+  }
+
+    render() {
+      return (
+        <div>
+        <h4>Create wallet</h4>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Account username:
+            <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
+          </label><br />
+          <label>
+            Currency:
+            <select name="currency" value={this.state.currency} onChange={this.handleChange}>
+              <option value="PGG">PGG</option>
+              <option value="CAD">CAD</option>
+            </select>
+          </label>
+          <input type="submit" value="Submit"/>
+        </form>
+        
         <button onClick={this.props.onLoadWallets} className={this.props.classes.test}>
           click here to load wallets
         </button>
