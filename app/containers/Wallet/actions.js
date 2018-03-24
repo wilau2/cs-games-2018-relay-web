3b70aa240c8229f3f8897d7da5a9e86a719dcfe6
jfgauron <jfgauron@gmail.com>
2018-03-24 14:53:52 -0400

    first hour done

--- app/containers/Wallet/actions.js
+++ app/containers/Wallet/actions.js
@@ -1,7 +1,31 @@
-import { LOAD_WALLETS } from './constants';
+import { LOAD_WALLETS, CREATE_WALLET, DEPOSIT_TO_WALLET, FIND_WALLET_BY_ACCOUNT, SHOW_CURRENCY_AND_AMOUNT_WALLET, TOTAL_VALUE_WALLETS_BY_ACCOUNT, TRADE_PRICE_WALLET } from './constants';
 
-export function loadWallets() {
-  return {
-    type: LOAD_WALLETS,
-  };
-}
+export const loadWallets = () => ({
+  type: LOAD_WALLETS,
+});
+
+export const createWallet = () => ({
+  type: CREATE_WALLET,
+  payload: {},
+});
+
+export const depositeToWallet = () => ({
+  type: DEPOSIT_TO_WALLET,
+});
+
+export const findWalletByAccount = () => ({
+  type: FIND_WALLET_BY_ACCOUNT,
+});
+
+export const showCurrenyAndAmountWallet = () => ({
+  type: SHOW_CURRENCY_AND_AMOUNT_WALLET,
+});
+
+export const totalValueWalletsByAccount = () => ({
+  type: TOTAL_VALUE_WALLETS_BY_ACCOUNT,
+});
+
+export const tradePriceWallet = () => ({
+  type: TRADE_PRICE_WALLET,
+});
+
--- app/containers/Wallet/constants.js
+++ app/containers/Wallet/constants.js
@@ -1,2 +1,8 @@
 export const LOAD_WALLETS = 'crypto_exchange/App/LOAD_WALLETS';
+export const CREATE_WALLET = 'crypto_exchange/App/CREATE_WALLETS';
+export const FIND_WALLET_BY_ACCOUNT = 'crypto_exchange/App/FIND_WALLET_BY_ACCOUNT';
+export const DEPOSIT_TO_WALLET = 'crypto_exchange/App/DEPOSIT_TO_WALLET';
+export const SHOW_CURRENCY_AND_AMOUNT_WALLET = 'crypto_exchange/App/SHOW_CURRENCY_AND_AMOUNT_WALLET';
+export const TRADE_PRICE_WALLET = 'crypto_exchange/App/TRADE_PRICE_WALLET';
+export const TOTAL_VALUE_WALLETS_BY_ACCOUNT = 'crypto_exchange/App/TOTAL_VALUE_WALLETS_BY_ACCOUNT';
 export const WALLETS = 'wallets';
