cf2e91b387ac97d441033a88f103bec3507480b8
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2018-03-24 16:55:55 -0400

    almost show the trades !

--- app/containers/Trades/index.js
+++ app/containers/Trades/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 
+import {loadTrades} from './actions';
+import reducer from './reducer';
+import injectReducer from '../../utils/injectReducer';
+
 
 class Trades extends React.Component {
 
+      /* Pas eu le temps, mais le reducer `trades` met le state dans le store so on peux juste maintenant afficher toute ici une fois que le store se fait peupler. */
+      /* PLS CONSIDER <3*/
   render() {
     return (
       <h2>Trades component</h2>
@@ -13,34 +19,39 @@ class Trades extends React.Component {
 }
 
 
-export const mapStateToProps = createStructuredSelector({
-  wallets: makeSelectWallets(),
-  walletsLoading: makeSelectLoading(selectWallets),
-  walletsError: makeSelectError(selectWallets),
-});
+export const mapStateToProps = state => {
+  return {
+    trades: state.trades,
+  }
+};
 
 export const mapDispatchToProps = (dispatch, ownProps) => ({
-  onLoadWallets: (evt) => {
-    if (evt !== undefined && evt.preventDefault) evt.preventDefault();
-    dispatch(loadWallets());
-  },
+  // TODO: Faire le call, pis juste afficher toute dans render
+  //onLoadWallets: dispatch()
+  //},
 });
 
 const withConnect = connect(mapStateToProps, mapDispatchToProps);
 
+//const withSurveyReducer = injectReducer({
+  //key: WALLETS,
+  //reducer,
+//});
+//
+//
 const withSurveyReducer = injectReducer({
-  key: WALLETS,
+  key: "trades",
   reducer,
 });
 
-const withSurveySaga = injectSaga({
-  key: WALLETS,
-  saga,
-});
+//const withSurveySaga = injectSaga({
+  //key: WALLETS,
+  //saga,
+//});
 
 
 export default compose(
   withSurveyReducer,
-  withSurveySaga,
+  //withSurveySaga,
   withConnect,
-)(withStyles(styles)(Wallet));
+)(Trades);
